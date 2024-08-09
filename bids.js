d3de1f37f1f02981a8099667a7027cf14761b76c
Torreyw94 <145612792+Torreyw94@users.noreply.github.com>
2024-08-09 08:39:45 -0700

    Add files via upload

---
+++ bids.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } =
+require('sequelize');
+
+class Bid extends Model {
+    static associate(models) {
+        this.belongsTo(models.User);
+        this.belongsTo(models.Job);
+    }
+}
+
+module.exports = ('sequelize') => {
+    Bid.init({
+        amount: DataTypes.DECIMAL,
+    }, {
+        sequelize,
+        modelName: 'Bid'
+});
+return Bid;
+};
